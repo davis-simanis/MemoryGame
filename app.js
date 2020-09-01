0348d5dd3c3f4c866074ebe8f01d3dcd7a8fa2af
Rudolfs <rudolfs.simanis@scandiweb.com>
2020-09-01 13:07:23 +0300

    Game is functioning. Minor bugs exist

--- app.js
+++ app.js
@@ -1,106 +1,163 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 // available cards
-const cardArray = [
+const imageArray = [
     {
-        name: 'city',
         img: 'images/city-q-c-100-100-8.jpg'
     },
     {
-        name: 'bread',
         img: 'images/food-q-c-100-100-10.jpg'
     },
     {
-        name: 'sunset',
         img: 'images/nature-q-c-100-100-5.jpg'
     },
     {
-        name: 'orchid',
         img: 'images/nature-q-c-100-100-7.jpg'
     },
     {
-        name: 'bar',
         img: 'images/nightlife-q-c-100-100-7.jpg'
     },
     {
-        name: 'ladies',
         img: 'images/people-q-c-100-100-4.jpg'
     },
     {
-        name: 'dj',
         img: 'images/technics-q-c-100-100-2.jpg'
     },
     {
-        name: 'bus',
         img: 'images/transport-q-c-100-100-4.jpg'
-    },
-    {
-        name: 'city',
-        img: 'images/city-q-c-100-100-8.jpg'
-    },
-    {
-        name: 'bread',
-        img: 'images/food-q-c-100-100-10.jpg'
-    },
-    {
-        name: 'sunset',
-        img: 'images/nature-q-c-100-100-5.jpg'
-    },
-    {
-        name: 'orchid',
-        img: 'images/nature-q-c-100-100-7.jpg'
-    },
-    {
-        name: 'bar',
-        img: 'images/nightlife-q-c-100-100-7.jpg'
-    },
-    {
-        name: 'ladies',
-        img: 'images/people-q-c-100-100-4.jpg'
-    },
-    {
-        name: 'dj',
-        img: 'images/technics-q-c-100-100-2.jpg'
-    },
-    {
-        name: 'bus',
-        img: 'images/transport-q-c-100-100-4.jpg'
-    },
-]
+    }
+];
 
-cardArray.sort(() => 0.5 - Math.random());
+const cardsArray = [];
+initializeCards(imageArray, cardsArray);
 
 const grid = document.querySelector('.grid');
 const result = document.querySelector('#result');
-const cardsChosen = [];
-const cardsChosenId = [];
-const cardsRevealed = [];
+let cardsChosen = [];
+const cardsMatched = [];
 
 //create card board
-function createBoard(cardArray) {
-    for (let i = 0; i < cardArray.length; i++) {
-        const card = document.createElement('img');
-        card.setAttribute('src', 'images/purple-square.jpg');
-        card.setAttribute('data-id', i);
-        card.addEventListener('click', flipCard);
-        grid.appendChild(card);
-    }
+function createBoard(cardsArray) {
+    cardsArray.forEach( card => {
+        const domCard = document.createElement('img');
+        domCard.setAttribute('src', card.getDisplayImage());
+        domCard.setAttribute('id', `card${card.getId()}`);
+        domCard.addEventListener('click', checkForMatch);
+        grid.appendChild(domCard);
+    })
 }
 
-// flip the chosen card
-function flipCard() {
-    const cardId = this.getAttribute('data-id');
-    const cards
-    this.setAttribute('src', cardArray[cardId].img);
-    cardsChosen.push
+function updateDisplayImage(array) {
+
+    array.forEach(card => {
+        domCard = document.querySelector(`#card${card.getId()}`);
+        domCard.setAttribute('src', card.getDisplayImage())
+    });
+
+}
+
+function updateScore(){
+    const result = document.querySelector('#result');
+    result.textContent = cardsMatched.length;
 }
 
 // check for match
-function checkForMatch(cardsChosen) {
+function checkForMatch() {
+    const domCard = this;
+    const card = getCard(domCard.getAttribute('id').slice(4));
+    card.flip();
+    cardsChosen.push(card);
 
+    updateDisplayImage(cardsChosen);
 
+    if(cardsChosen.length === 2) {
+        setTimeout(() => {
+            if (cardsChosen[0].image === cardsChosen[1].image) {
+                cardsChosen[0].markMatched();
+                cardsChosen[1].markMatched();
+                cardsMatched.push(cardsChosen[0]);
+                updateScore();
+            } else {
+                cardsChosen[0].flipBack();
+                cardsChosen[1].flipBack();
+            }
+            updateDisplayImage(cardsChosen);
+            cardsChosen = [];
+        },
+        400);
+    }
 }
 
-createBoard(cardArray);
+function getCard(cardId) {
+    let result = 'Card is not found';
+
+    cardsArray.forEach(card => {
+        if (card.getId() == cardId) {
+            result = card;
+        }
+    });
+
+    return result;
+}
+
+function initializeCards(imageArray, cardsArray) {
+    for (let i = 0; i < imageArray.length; i++) {
+        const pairFirstCardId = i;
+        const pairSecondCardId = (imageArray.length * 2) - 1 - i;
+
+        cardsArray[pairFirstCardId] = new Card(pairFirstCardId, imageArray[i].img);
+        cardsArray[pairSecondCardId] = new Card(pairSecondCardId, imageArray[i].img);
+    }
+
+    cardsArray.sort(() => 0.8 - Math.random());
+};
+
+createBoard(cardsArray);
 
 });
+
+class Card {
+    imageAfterMatched = 'images/blank-square.jpg';
+    imageBeforeMatched = 'images/purple-square.jpg';
+    displayImage = '';
+    image = '';
+    id = '';
+
+    constructor(id, image)  {
+        this.id = id;
+        this.image = image;
+        this.displayImage = this.imageBeforeMatched;
+    }
+
+    getDisplayImage() {
+        return this.displayImage;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    setImage(image) {
+        this.image = image;
+    }
+
+    flip() {
+        this.displayImage = this.image;
+    }
+
+    flipBack() {
+        this.displayImage = this.imageBeforeMatched
+    }
+
+    markMatched() {
+        this.displayImage = this.imageAfterMatched;
+    }
+}
